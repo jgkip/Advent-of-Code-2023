a2fb5113aa647538a82c455bac268a15c99584ab
Joseph Gonzaga <14jgonz@gmail.com>
2023-12-05 18:32:58 -0500

    day 5

--- day_5/main.py
+++ day_5/main.py
@@ -1,7 +1,22 @@
-from file_reading import read_file
+import os 
 
+def read_file(file_name: str):
+	# cwd: str = os.path.dirname(os.path.realpath)
+	file_path = os.getcwd() + "\\tests" + "\\" + file_name
+	f = open(file_path, "r")
+	return f
 
 def main():
+	file = read_file("zero.txt")
+	seeds = []
+	for line in file:
+		if line.startswith("seeds:"):
+			line = line[line.find(":")+1:]
+			seeds = line.split()
+		# delete plz
+		if line.startswith("seed-to-soil"):
+			#(soil, seed, range)
+
 
 
 if __name__ == "__main__":
